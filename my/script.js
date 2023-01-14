b8d8b1552d1ccf22ff48331d31acb04a9e4f8a6d
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2023-01-14 22:09:59 +0300

    Almost Admins table is done

--- my/script.js
+++ my/script.js
@@ -2,3 +2,22 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+
+const togglePassword = document
+  .querySelector('#togglePassword');
+
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', () => {
+
+  // Toggle the type attribute using
+  // getAttribure() method
+  const type = password
+    .getAttribute('type') === 'password' ?
+    'text' : 'password';
+
+  password.setAttribute('type', type);
+
+  // Toggle the eye and bi-eye icon
+  this.classList.toggle('bi-eye');
+});
